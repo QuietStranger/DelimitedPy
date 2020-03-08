11c73a1eb79315fb717c13a2c1a14c549aedb47d
QuietStranger <54193290+QuietStranger@users.noreply.github.com>
2020-03-08 14:50:24 -0500

    Update delimitedPy.py

--- delimitedPy.py
+++ delimitedPy.py
@@ -1,10 +1,20 @@
 from tkinter import *
 
+
+# Here, we are creating our class, Window, and inheriting from the Frame
+# class. Frame is a class from the tkinter module. (see Lib/tkinter/__init__)
 class Window(Frame):
 
+    # Define settings upon initialization. Here you can specify
     def __init__(self, master=None):
-        Frame.__init__(self, master)               
+        
+        # parameters that you want to send through the Frame class. 
+        Frame.__init__(self, master)   
+
+        #reference to the master widget, which is the tk window                 
         self.master = master
+
+        #with that, we want to then run init_window, which doesn't yet exist
         self.init_window()
 
     #Creation of init_window
@@ -17,19 +27,24 @@ class Window(Frame):
         self.pack(fill=BOTH, expand=1)
 
         # creating a button instance
-        quitButton = Button(self, text="Quit")
+        quitButton = Button(self, text="Exit",command=self.client_exit)
 
         # placing the button on my window
         quitButton.place(x=0, y=0)
 
+       
+    # definition for event
+    def client_exit(self):
+        exit()
 
+# root window created. Here, that would be the only window, but
+# you can later have windows within windows.
 root = Tk()
 
-
-#size of the window
 root.geometry("400x300")
 
+#creation of an instance
 app = Window(root)
 
+#mainloop 
 root.mainloop()
-
