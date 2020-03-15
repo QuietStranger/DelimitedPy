2af6ebbdaf77f5dbe4ac8420c60048ed3069658f
QuietStranger <54193290+QuietStranger@users.noreply.github.com>
2020-03-14 23:22:51 -0500

    checkpoint

--- Custom_Methods.py
+++ Custom_Methods.py
@@ -58,4 +58,29 @@ def check_utf8():
     # print('singleCharacter' + str(singleCharacter.encode(encoding='UTF-8', errors='strict')))
 
     print(multipleChars.encode(encoding='us-ascii', errors='strict'))
-check_utf8()
+
+
+def get_headers_csv(csvFilePath='./test_csv.csv'):
+    # if not csvFilePath:
+    #     # headers = 'error'
+    #     pass
+    # else:
+    #     csvFilePath = str(csvFilePath)
+    
+    # csvFilePath = Path(csvFilePath)
+    with open(csvFilePath, newline='') as csvFile:
+        reader = csv.reader(csvFile, delimiter=',', quotechar='"')
+        
+
+        for row in reader:
+            print(row)
+
+        print(reader.)
+        
+        # .writerow If you want to insert a row before copying.
+        # writer.writerow(['name', 'size', 'modified'])
+
+        # Actual copying line by line.
+        # for row in reader:
+        #     writer.writerow(row)
+get_headers_csv()
